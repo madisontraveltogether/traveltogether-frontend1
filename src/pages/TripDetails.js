4e01a1b46a2fa538c5c256fc861a24cee3ad3c1c
Madison <mhmadisonhenry@gmail.com>
2024-11-21 18:32:19 -0500

    updating a route in tripdetails

--- src/pages/TripDetails.js
+++ src/pages/TripDetails.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import api from '../services/api';
-import '../css/TripDetails.css';
+import '../css/TripDashboard.css';
 import TopBar from '../components/TopBar';
 import BottomNav from '../components/BottomNav';
 
