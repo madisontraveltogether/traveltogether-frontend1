3c234ddff573ed84ad00d2ddb726d00e0e9ab1cb
madisontraveltogether <madison@gettraveltogether.com>
2024-11-22 17:25:24 -0500

    another route link fix

--- src/pages/TripDetails.js
+++ src/pages/TripDetails.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import api from "../services/api";
-import "../css/TripDashboard"
+import "./css/TripDashboard"
 const TripDetails = () => {
   const { tripId } = useParams();
   const [trip, setTrip] = useState({});
