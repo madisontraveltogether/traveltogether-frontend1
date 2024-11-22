6db251a2b716c5c77426a8f94500763a9ea0bfb7
madisontraveltogether <madison@gettraveltogether.com>
2024-11-22 17:23:12 -0500

    fixing wrong link

--- src/pages/TripDetails.js
+++ src/pages/TripDetails.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import api from "../services/api";
-import "../css/TripDetails.css";
+import "../css/TipDashboard.css";
 
 const TripDetails = () => {
   const { tripId } = useParams();
