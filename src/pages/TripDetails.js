e5a9350d558d18ff60cff79e0db6a94d38a33b6d
madisontraveltogether <madison@gettraveltogether.com>
2024-11-22 17:22:09 -0500

    going back to functionality

--- src/pages/TripDetails.js
+++ src/pages/TripDetails.js
@@ -1,296 +1,46 @@
-import React from 'react';
-import '../css/TripDashboard.css';
+import React, { useEffect, useState } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import api from "../services/api";
+import "../css/TripDetails.css";
 
-export default function Main() {
+const TripDetails = () => {
+  const { tripId } = useParams();
+  const [trip, setTrip] = useState({});
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    const fetchTripDetails = async () => {
+      try {
+        const response = await api.get(`/api/trips/${tripId}`);
+        setTrip(response.data);
+      } catch (err) {
+        setError("Failed to load trip details.");
+      }
+    };
+
+    fetchTripDetails();
+  }, [tripId]);
+
+  if (!trip) return <p>Loading...</p>;
+
   return (
-    <div className='main-container'>
-      <div className='rectangle'>
-        <div className='ellipse' />
-        <div className='group' />
+    <div className="trip-container">
+      <div className="cover-photo">
+        <img src="https://via.placeholder.com/428x200" alt="Cover" />
       </div>
-      <div className='rectangle-1' />
-      <div className='frame'>
-        <div className='frame-2'>
-          <div className='title-location'>
-            <div className='frame-3'>
-              <button className='button-frame'>
-                <div className='frame-4'>
-                  <div className='date-display'>
-                    <span className='date-text'>April 7th - 10th, 2023</span>
-                    <div className='rectangle-5' />
-                  </div>
-                </div>
-              </button>
-              <div className='group-6' />
-            </div>
-            <span className='bachelorette-title'>
-              Jacqueline’s Bachelorette!
-            </span>
-            <div className='frame-7'>
-              <div className='frame-8'>
-                <div className='location-display'>
-                  <span className='location-text'>Las Vegas, Nevada</span>
-                  <div className='location-icon'>
-                    <div className='vector' />
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <span className='party-description'>
-            We’re kicking off Jackie’s marriage the best way we know how: Vegas,
-            baby! Let’s make her bachelorette party one she’ll definitely
-            forget. 😉
-          </span>
-        </div>
-        <div className='group-9'>
-          <div className='avatar-name'>
-            <div className='ellipse-a' />
-            <span className='organizer-name'>Mai Alexander</span>
-          </div>
-          <span className='organized-by'>Organized by</span>
+
+      <div className="trip-details">
+        <h1>{trip.name || "Trip Name"}</h1>
+        <p>{trip.description || "Trip Description"}</p>
+        <div className="trip-info">
+          <span>{trip.location || "Location"}</span>
+          <span>{trip.dates || "Dates"}</span>
         </div>
       </div>
-      <div className='rectangle-b'>
-        <span className='attendees-title'>Attendees</span>
-        <div className='flex-row-bd'>
-          <span className='attendee'>2</span>
-          <span className='attendee-c'>10</span>
-          <span className='attendee-d'>5</span>
-        </div>
-        <div className='flex-row-cf'>
-          <span className='going'>Going</span>
-          <span className='maybe'>Maybe</span>
-          <span className='invited'>Invited</span>
-        </div>
-        <div className='flex-row'>
-          <div className='ellipse-e' />
-          <div className='ellipse-f' />
-          <div className='ellipse-10' />
-          <div className='ellipse-11' />
-          <div className='ellipse-12' />
-          <div className='ellipse-13' />
-          <div className='ellipse-14' />
-          <div className='ellipse-15' />
-          <div className='ellipse-16' />
-          <div className='ellipse-17' />
-          <div className='ellipse-18' />
-          <div className='ellipse-19' />
-          <div className='ellipse-1a' />
-          <span className='plus-five'>+5</span>
-        </div>
-      </div>
-      <div className='rectangle-1b'>
-        <div className='announcement-card'>
-          <div className='frame-1c'>
-            <div className='frame-1d'>
-              <div className='group-1e'>
-                <div className='ellipse-1f' />
-                <span className='natasha-lim'>Natasha Lim</span>
-              </div>
-              <span className='december-15th'>
-                December 15th, 2023 @ 5:32pm
-              </span>
-            </div>
-            <div className='charm-menu-kebab'>
-              <div className='group-20'>
-                <div className='vector-21' />
-                <div className='vector-22' />
-                <div className='vector-23' />
-              </div>
-            </div>
-          </div>
-          <div className='frame-24'>
-            <span className='aw-shoot'>
-              Aw shoot! This actually falls on the same date as my sister’s
-              wedding. I would’ve loved to come though. Have fun girls!
-            </span>
-            <div className='frame-25'>
-              <div className='frame-26'>
-                <div className='frame-27'>
-                  <span className='replies'>2 Replies</span>
-                  <div className='iconoir-reply'>
-                    <div className='vector-28' />
-                  </div>
-                </div>
-                <div className='group-29'>
-                  <div className='group-2a'>
-                    <div className='fluent-emoji-add-regular'>
-                      <div className='vector-2b' />
-                    </div>
-                    <div className='rectangle-2c' />
-                  </div>
-                  <div className='group-2d'>
-                    <div className='fluent-emoji-add-regular-2e'>
-                      <div className='vector-2f' />
-                    </div>
-                    <div className='rectangle-30' />
-                  </div>
-                  <div className='group-31'>
-                    <div className='rectangle-32' />
-                    <span className='number-2'>2</span>
-                    <div className='emoji-crying' />
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className='announcement-card-33'>
-          <div className='frame-34'>
-            <div className='frame-35'>
-              <div className='group-36'>
-                <div className='ellipse-37' />
-                <span className='name-natasha-lim-michel-robbins'>
-                  Michel Robbins
-                </span>
-              </div>
-              <span className='date-mar-1st-3-29pm'>
-                
-                Mar 1st, 2023 @ 3:29pm
-              </span>
-            </div>
-            <div className='charm-menu-kebab-38'>
-              <div className='group-39'>
-                <div className='vector-3a' />
-                <div className='vector-3b' />
-                <div className='vector-3c' />
-              </div>
-            </div>
-          </div>
-          <div className='frame-3d'>
-            <div className='message-i-got-our-rooms-at-the-bellagio'>
-              <span className='message-i-got-our-rooms-at-the-bellagio-3e'>
-                I got our rooms at the Bellagio!
-              </span>
-              <span className='mention-mai-alexander'>@Mai Alexander</span>
-              <span className='message-let-me-know-if-you-need-any-help-decorating'>
-                
-                Let me know if you need any help decorating since I’ll be there
-                a few days early and can grab supplies.
-              </span>
-              <span className='line-break'>
-                <br />
-                <br />
-              </span>
-            </div>
-            <div className='frame-3f'>
-              <div className='frame-40'>
-                <div className='frame-41'>
-                  <span className='reply-section'>2 Replies</span>
-                  <div className='reply-icon'>
-                    <div className='vector-42' />
-                  </div>
-                </div>
-                <div className='group-43'>
-                  <div className='group-44'>
-                    <div className='emoji-add'>
-                      <div className='vector-45' />
-                    </div>
-                    <div className='rectangle-46' />
-                  </div>
-                  <div className='group-47'>
-                    <div className='emoji-add-48'>
-                      <div className='vector-49' />
-                    </div>
-                    <div className='rectangle-4a' />
-                  </div>
-                  <div className='group-4b'>
-                    <div className='rectangle-4c' />
-                    <span className='reply-count'>2</span>
-                    <div className='praying-hands' />
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className='rectangle-4d'>
-          <div className='rectangle-4e'>
-            <div className='flex-row-4f'>
-              <div className='home-icon'>
-                <div className='vector-50' />
-              </div>
-              <div className='map-icon'>
-                <div className='vector-51' />
-              </div>
-              <div className='money-icon' />
-              <div className='chat-icon'>
-                <div className='vector-52' />
-              </div>
-            </div>
-            <div className='flex-row-53'>
-              <span className='trip-home-54'>Trip Home</span>
-              <span className='plans'>Plans</span>
-              <span className='expenses'>Expenses</span>
-              <span className='messages'>Messages</span>
-            </div>
-          </div>
-        </div>
-        <div className='announcement-card-55'>
-          <div className='frame-56'>
-            <div className='frame-57'>
-              <div className='group-58'>
-                <div className='ellipse-59' />
-                <span className='mai-alexander'>Mai Alexander</span>
-              </div>
-              <span className='january-16th-2023'>
-                January 16th, 2023 @ 2:49pm
-              </span>
-            </div>
-            <div className='charm-menu-kebab-5a'>
-              <div className='group-5b'>
-                <div className='vector-5c' />
-                <div className='vector-5d' />
-                <div className='vector-5e' />
-              </div>
-            </div>
-          </div>
-          <div className='frame-5f'>
-            <span className='information'>
-              Here is the information about the lorem ipsum dolor sit amet
-              constructeur
-              <br />
-              <br />
-            </span>
-            <div className='frame-60'>
-              <div className='frame-61'>
-                <div className='frame-62'>
-                  <span className='replies-63'>2 Replies</span>
-                  <div className='iconoir-reply-64'>
-                    <div className='vector-65' />
-                  </div>
-                </div>
-                <div className='group-66'>
-                  <div className='group-67'>
-                    <div className='fluent-emoji-add-regular-68'>
-                      <div className='vector-69' />
-                    </div>
-                    <div className='rectangle-6a' />
-                  </div>
-                  <div className='group-6b'>
-                    <div className='fluent-emoji-add-regular-6c'>
-                      <div className='vector-6d' />
-                    </div>
-                    <div className='rectangle-6e' />
-                  </div>
-                  <div className='group-6f'>
-                    <div className='rectangle-70' />
-                    <span className='number'>5</span>
-                    <div className='laughing' />
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className='vector-71' />
-      </div>
-      <span className='announcements'>Announcements</span>
-      <div className='ellipse-72' />
-      <div className='material-symbols-add'>
-        <div className='vector-73' />
-      </div>
+
+      {error && <p className="error-message">{error}</p>}
     </div>
   );
-}
+};
+
+export default TripDetails;
